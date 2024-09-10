3e17da74ee8223d09b66dccfc14dacde21aacac2
khan_root <sarmadf49@gmail.com>
2024-09-10 23:32:22 +0500

    auth Route Define

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express, {Express, Request, Response} from 'express'
 import { PORT } from './secret';
+import rootRouter from './routes';
 
 const app: Express = express();
 
-app.get('/check-health', (req: Request, res: Response)=>{
-    res.send('Working')
-})
+app.use('/api', rootRouter)
 
 
 app.listen(PORT, ()=>{
